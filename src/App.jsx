b670d6eda13ecf0964dad78cbc834b2ad7903d51
Arun <arunm102301@gmail.com>
2025-01-08 18:44:15 +0530

    todo design

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react';  // Import the React library
+import './App.css';  // Import the CSS file
 
+// Create a functional component called App
 function App() {
 
+  // Return the JSX for the App component
   return (
-    <>
-      <h1> welcome</h1>  
-    </>
+    <main>
+      <h1>To Do List</h1>
+
+      <div className='input-container'>
+        <input type="text" />
+        <button>Add</button>  
+      </div>
+
+      <div className="container">
+        <div className="todo">
+          <p>task list</p>
+          <div className="actions">
+            <input type="checkbox" />
+            <button>Delete</button>
+          </div>
+        </div>
+      </div>
+    </main>
   )
 }
 
